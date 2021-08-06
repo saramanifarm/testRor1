06d7fbeefab11d0e2931f0859f8ead6947f994aa
saramanifarm <saraman@i.farm>
2021-08-06 10:42:20 +0530

    crud done

--- app/controllers/artis_controller.rb
+++ app/controllers/artis_controller.rb
@@ -6,6 +6,43 @@ class ArtisController < ApplicationController
     end
 
     def index
-        @articals = Artis.all
+        @artis = Artis.all
+    end
+
+    def new
+        @artis = Artis.new
+    end
+
+    def edit
+        #byebug
+        @artis = Artis.find(params[:id])
+    end
+
+    def update
+        @artis = Artis.find(params[:id])
+       if @artis.update(params.require(:artis).permit(:title, :description))
+        flash[:notice] = "Artical update successfully...."
+        redirect_to @artis
+       else
+        render 'edit'
+       end
+    end
+
+    def create
+        @artis = Artis.new(params.require(:arti).permit(:title, :description))
+        #render plain: @artis.inspect
+         if @artis.save
+            flash[:notice] = "Articles was created succesfully....."
+            redirect_to @artis
+         else
+            render 'new'
+         end
+        
+    end
+
+    def destroy
+        @artis = Artis.find(params[:id])
+        @artis.destroy
+        redirect_to artis_path
     end
 end
